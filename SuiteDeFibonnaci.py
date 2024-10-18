726913070ca0957b5838816bd1e9bd74860a427c
letslove <163998252+letslovesailors@users.noreply.github.com>
2024-10-18 10:18:59 +0200

    Add files via upload

---
+++ SuiteDeFibonnaci.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+        tab =[]
+        for I in range(50):
+            n=0
+            n1=n-1
+            n2=n+1
+            n=n1+n2
+            return(tab[n])
+tab = []
+tab.append(50)
+print(tab)
+
+
+
+
+
+        
+                
+                
+
