fb50ab879362499f41dc1c89e04e4f437f6c11d8
letslove <163998252+letslovesailors@users.noreply.github.com>
2024-10-24 09:58:33 +0200

    Update def divis(n):.py

--- def divis(n):.py
+++ def divis(n):.py
@@ -1,8 +1,8 @@
+p=int(input)("saisir un nombre entier")
 def divis(n):
-    n=input("Saisir un nombre") 
     if n%11== 0:
         C="Ce nombre est divisible par 11"
     else:
         C="Ce nombre n'est pas divisible par 11"
     return C
-print(divis(n))
+print(divis(p))
