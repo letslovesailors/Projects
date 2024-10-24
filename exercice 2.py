d6838fdd36067d83fda01c1a991963bf6e5d7e65
letslove <163998252+letslovesailors@users.noreply.github.com>
2024-10-24 10:49:50 +0200

    Update exercice 2.py

--- exercice 2.py
+++ exercice 2.py
@@ -1,8 +1,12 @@
 from math import *
-a = int(input())
+
+# Demande à l'utilisateur de saisir
+a = int(input("Saisir un nombre de photocopies: "))
+
 def prix(n):
     if n <= 50:
-        n == 0.15
+        return 0.15 * n
     elif n >= 51:
-        n == 0.10
-    return(n)
+        return 0.10 * n  
+resultat = prix(a)
+print("Le prix des photocopies est:", resultat)
